30eb2cbe2bcd6653ca3245ca75d9d5243343f612
Prateek Roy <royprateek96@gmail.com>
2019-10-24 01:23:48 +0530

    animation update

--- script.js
+++ script.js
@@ -1,4 +1,92 @@
+let animation = function () {
+
+  var pos = $(window).scrollTop();
+  var wh = $(window).height();
+  //console.log(wh)
+
+  $('.slideanim').each(function () {
+    let t = $(this).offset().top;
+    if (t < pos + 0.62 * wh) {
+      if (!$(this).hasClass('animated')) {
+        $(this).css('visibility', 'visible');
+        $(this).addClass('animated fadeIn');
+
+        //console.log('class added')
+      }
+    }
+  })
+
+  $('.contactanim').each(function () {
+    let t = $('#contact').offset().top;
+    if (t < pos + 0.62 * wh) {
+      let a = parseInt($(this).attr('a'));
+      let anim = $(this).attr('anim');
+      if (!$(this).hasClass('animated')) {
+        $(this).css('visibility', 'visible');
+        $(this).css('animation-delay', a * 200 + 'ms');
+        $(this).addClass('animated ' + anim);
+      }
+    }
+  })
+
+  $('.skillsanim').each(function () {
+    let t = $('#skills').offset().top;
+    if (t < pos + 0.62 * wh) {
+      if ($(this).attr('a') == 'r') {
+        let a = 3 + Math.floor(Math.random() * Math.floor(3));
+        let anim = $(this).attr('anim');
+        if (!$(this).hasClass('animated')) {
+          $(this).css('visibility', 'visible');
+          $(this).css('animation-delay', a * 500 + 'ms');
+          $(this).addClass('animated ' + anim);
+        }
+      }
+      else {
+        //console.log('else')
+        let a = parseInt($(this).attr('a'));
+        let anim = $(this).attr('anim');
+        if (!$(this).hasClass('animated')) {
+          $(this).css('visibility', 'visible');
+          $(this).css('animation-delay', a * 500 + 'ms');
+          $(this).addClass('animated ' + anim);
+        }
+      }
+    }
+
+  });
+
+  $('.projectsanim').each(function () {
+    let t = $('#projects').offset().top;
+    if (t < pos + 0.62 * wh) {
+      let a = parseInt($(this).attr('a'));
+      let anim = $(this).attr('anim');
+      if (!$(this).hasClass('animated')) {
+        $(this).css('visibility', 'visible');
+        $(this).css('animation-delay', a * 200 + 'ms');
+        $(this).addClass('animated ' + anim);
+      }
+    }
+  })
+
+  $('.aboutanim').each(function () {
+    let t = $('#about').offset().top;
+    if (t < pos + 0.62 * wh) {
+      let a = parseInt($(this).attr('a'));
+      let anim = $(this).attr('anim');
+      if (!$(this).hasClass('animated')) {
+        $(this).css('visibility', 'visible');
+        $(this).css('animation-delay', a * 200 + 'ms');
+        $(this).addClass('animated ' + anim);
+      }
+    }
+  })
+
+
+
+}
+
 $(function () {
+
 
   $('.scrollspy').scrollSpy({
     scrollOffset: 10
@@ -17,97 +105,8 @@ $(function () {
 
   //Popanim
 
-  $(window).on('scroll', function () {
-
-    var pos = $(window).scrollTop();
-    var wh = $(window).height();
-    //console.log(wh)
-
-    $('.slideanim').each(function () {
-      let t = $(this).offset().top;
-      if (t < pos + 0.62 * wh) {
-        if (!$(this).hasClass('animated')) {
-          $(this).css('visibility', 'visible');
-          $(this).addClass('animated slideInRight');
-
-          //console.log('class added')
-        }
-      }
-    })
-
-    $('.contactanim').each(function () {
-      let t = $('#contact').offset().top;
-      if (t < pos + 0.62 * wh) {
-        let a = parseInt($(this).attr('a'));
-        let anim = $(this).attr('anim');
-        if (!$(this).hasClass('animated')) {
-          $(this).css('visibility', 'visible');
-          $(this).css('animation-delay', a * 200 + 'ms');
-          $(this).addClass('animated ' + anim);
-        }
-      }
-    })
-
-    $('.skillsanim').each(function () {
-      let t = $('#skills').offset().top;
-      if (t < pos + 0.62 * wh) {
-        if ($(this).attr('a') == 'r') {
-          let a = 3 + Math.floor(Math.random() * Math.floor(3));
-          let anim = $(this).attr('anim');
-          if (!$(this).hasClass('animated')) {
-            $(this).css('visibility', 'visible');
-            $(this).css('animation-delay', a * 500 + 'ms');
-            $(this).addClass('animated ' + anim);
-          }
-        }
-        else {
-          //console.log('else')
-          let a = parseInt($(this).attr('a'));
-          let anim = $(this).attr('anim');
-          if (!$(this).hasClass('animated')) {
-            $(this).css('visibility', 'visible');
-            $(this).css('animation-delay', a * 500 + 'ms');
-            $(this).addClass('animated ' + anim);
-          }
-        }
-      }
-
-    });
-
-    $('.projectsanim').each(function () {
-      let t = $('#projects').offset().top;
-      if (t < pos + 0.62 * wh) {
-        let a = parseInt($(this).attr('a'));
-        let anim = $(this).attr('anim');
-        if (!$(this).hasClass('animated')) {
-          $(this).css('visibility', 'visible');
-          $(this).css('animation-delay', a * 200 + 'ms');
-          $(this).addClass('animated ' + anim);
-        }
-      }
-    })
-
-    $('.aboutanim').each(function () {
-      let t = $('#about').offset().top;
-      if (t < pos + 0.62 * wh) {
-        let a = parseInt($(this).attr('a'));
-        let anim = $(this).attr('anim');
-        if (!$(this).hasClass('animated')) {
-          $(this).css('visibility', 'visible');
-          $(this).css('animation-delay', a * 200 + 'ms');
-          $(this).addClass('animated ' + anim);
-        }
-      }
-    })
-
-
-
-  })
-
-
-
-
-
+  animation();
+  $(window).on('scroll', animation)
 
 })
 
